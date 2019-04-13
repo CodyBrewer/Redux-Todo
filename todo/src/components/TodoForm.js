93f220f05821617ab44722c4d80ed7a83b3fc187
CodyBrewer <brewer.cody.m@gmail.com>
2019-04-12 19:10:49 -0600

    create react app and intial boilerplate

---
+++ todo/src/components/TodoForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TodoForm = () => {
+    return( 
+        <div>
+            <form action="">
+                <input type="text" name="" id=""/>
+                <button type="submit">Add Todo</button>
+            </form>
+        </div>
+    )
+}
+export default TodoForm;
