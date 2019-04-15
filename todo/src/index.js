522d7b6e1a528d67c54c5742f03a507a9396a4c7
CodyBrewer <brewer.cody.m@gmail.com>
2019-04-15 11:34:49 -0600

    mvp completed

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from './reducers'
-import './index.css';
-import App from './App';
-
+import App from './components/App'
 
 const store = createStore(rootReducer)
 
@@ -15,8 +13,3 @@ render(
   </Provider>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
